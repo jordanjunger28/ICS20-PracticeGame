d257f0f7a78da7a6adac00dcbdd4890fa3dc86cb
jordanjunger28 <47332267+jordanjunger28@users.noreply.github.com>
2019-03-21 13:42:08 -0400

    Update person.js

--- person.js
+++ person.js
@@ -3,23 +3,29 @@ function Person() {
   this.vel = createVector(1, 0);
   this.acc = createVector(0, 0);
   this.mass = 10;
-this.applyForce=function(force){
-  this.acc.add(force);
-}
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
   this.update = function() {
-  this.vel.add(this.acc);
-  this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
- this.display = function() {
+  this.display = function() {
     fill(255, 150);
     stroke(255);
     rect(this.pos.x, this.pos.y, this.mass*10, this.mass*10);
   }
   this.edges = function() {
     if (this.pos.y > 250) {
-      this.vel.y *= -0;
+      this.vel.y *= 0;
       this.pos.y = 250;
     }
   }
+
+this.health = function(){
+	this.mass--;
+ }
+	
+	
 }
