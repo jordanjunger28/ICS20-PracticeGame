f7f7dd3f8664b45a372e520c6cca76abe2e0a3da
jordanjunger28 <47332267+jordanjunger28@users.noreply.github.com>
2019-03-22 15:21:54 -0400

    Update person.js

--- person.js
+++ person.js
@@ -1,31 +1,27 @@
 function Person() {
-  this.pos = createVector(50, 250);
-  this.vel = createVector(1, 0);
-  this.acc = createVector(0, 0);
+  this.pos = createVector(100, 250);
+  this.vel = createVector(1, 2);
+  this.acc = createVector(1, 2);
   this.mass = 10;
   this.applyForce = function(force) {
     this.acc.add(force);
-  }
+  background(20,15,200);
+	}
+	
   this.update = function() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
   this.display = function() {
-    fill(255, 150);
-    stroke(255);
+    fill(300, 150);
+    stroke(200);
     rect(this.pos.x, this.pos.y, this.mass*10, this.mass*10);
-  }
-  this.edges = function() {
+   text(" Jump over rectangle to score a point);
+	
+				{	
+		this.edges = function() {
     if (this.pos.y > 250) {
       this.vel.y *= 0;
       this.pos.y = 250;
-    }
-  }
-
-this.health = function(){
-	this.mass--;
- }
-	
-	
-}
+    
