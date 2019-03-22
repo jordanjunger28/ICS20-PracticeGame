2740e13b71efe936c8ef37779d1b41930cb3f8fb
jordanjunger28 <47332267+jordanjunger28@users.noreply.github.com>
2019-03-22 15:22:37 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,14 +18,17 @@ function keyPressed(){
 function draw() {
   
 	if (sceneNum===0){
-		textSize(20);
-		fill(50,200,30);
-		text("2D Side Scroller",100,100);
-		fill(200,200,200);
+		background(50,100,200)
+		textSize(30);
+	  fill(50,200,30);
+		text("2D Side Scroller",90,90);
+		textSize(30);
+		fill(15,200,200);
 		text("press q to play",200,200);
 		
+	
 	}else if(sceneNum===1){
-	background(51);
+	background(50);
   translate(-person.pos.x,0);
   var gravity = createVector(0,0.1);
   person.applyForce(gravity);
@@ -35,14 +38,16 @@ function draw() {
   person.applyForce(force);
 	}
 	//translate(-person.pos.x,0);
-    person.update();
+   background(30,100,200)
+		person.update();
   person.edges();
     person.display();
-  fill(255,0,100);
-  rect(400, height-50,50,50);
+   fill(255,30,100);
+  rect(500, height-18,100,80);
   }else{
-	 background(50,100,200);
-	 fill(130,100,30)
-	 text("That is all folks",200,200);
-	}
-}	
+	 background(50,150,250);
+	 fill(50,100,200)
+	 background(30,100,250);
+		text("That is all folks",180,180);
+	 }
+}
