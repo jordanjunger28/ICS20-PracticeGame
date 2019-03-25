eeccde0b6f1b7d1da6cc6aee6d94685e50c6b826
jordanjunger28 <47332267+jordanjunger28@users.noreply.github.com>
2019-03-25 13:20:13 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var person;
-var sceneNum=0
+var sceneNum=0;
 
 function setup() {
   createCanvas(640, 360);
@@ -10,44 +10,46 @@ function keyPressed(){
   if (key == ' '){
     var jump = createVector(0,-5);
     person.applyForce(jump);
-	} else if(key=='q'){
+    person.count();
+  } else if (key=='q'){
     sceneNum++;
-	}
-}		
+  }
+}
 
 function draw() {
   
-	if (sceneNum===0){
-		background(50,100,200)
-		textSize(30);
-	  fill(50,200,30);
-		text("2D Side Scroller",90,90);
-		textSize(30);
-		fill(15,200,200);
-		text("press q to play",200,200);
-		
-	
-	}else if(sceneNum===1){
-	background(50);
-  translate(-person.pos.x,0);
+  if(sceneNum===0){
+    background(200,50,50);
+    textSize(30);
+    fill(50,200,30);
+    
+    background(200,30,37)
+		text("2D Side Scroller",100,100);
+    fill(200,200,200);
+    textSize(18);
+    text("press q to play", 10,350);
+    
+  }else if(sceneNum===1){
+  background(51,100,100);
+  translate(-person.pos.x+100,0);
   var gravity = createVector(0,0.1);
   person.applyForce(gravity);
   if(mouseIsPressed){
   
-	var force = createVector(-0.1,0);
+  var force = createVector(-0.1,0);
   person.applyForce(force);
-	}
-	//translate(-person.pos.x,0);
-   background(30,100,200)
-		person.update();
+  }
+  //translate(-person.pos.x,0);
+    person.update();
   person.edges();
     person.display();
-   fill(255,30,100);
-  rect(500, height-18,100,80);
+  fill(300,10,150);
+  rect(300, height-150,50,50);
   }else{
-	 background(50,150,250);
-	 fill(50,100,200)
-	 background(30,100,250);
-		text("That is all folks",180,180);
-	 }
+    background(80,100,200);
+    fill(130,1,13);
+    textSize(50);
+    strokeWeight(10);
+    text("That is all folks!",50,200);
+  }
 }
